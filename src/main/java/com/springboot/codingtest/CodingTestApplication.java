1db237ddd3c0c36ede9d820daba43152c76870a1
rasmithe1 <rasmiranjan.pradhan01@gmail.com>
2021-07-22 04:07:57 +0530

    Code commit for both get and post services

--- src/main/java/com/springboot/codingtest/CodingTestApplication.java
+++ src/main/java/com/springboot/codingtest/CodingTestApplication.java
@@ -1,21 +1,20 @@
 package com.springboot.codingtest;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CodingTestApplication {
 
 	@Bean
-	public RestTemplate getRestTemplate() {
-		/*
-		 * HttpComponentsClientHttpRequestFactory httpFactory = new
-		 * HttpComponentsClientHttpRequestFactory();
-		 * httpFactory.setConnectTimeout(15000);// 15 Seconds return new
-		 */		return new RestTemplate();
+	public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.setConnectTimeout(Duration.ofMillis(15000)).setReadTimeout(Duration.ofMillis(35000))
+				.build();
 	}
 
 	public static void main(String[] args) {
